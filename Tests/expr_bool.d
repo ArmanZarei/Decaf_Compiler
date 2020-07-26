c072dfd382a175c34397f5502bb3e91fc00d4655
Arman <armanzarei1378@gmail.com>
2020-07-27 01:18:36 +0430

    Tests added

---
+++ Tests/expr_bool.d
@@ -0,0 +1,16 @@
+int main() {
+    bool a;
+    bool b;
+    bool c;
+    a = true;
+    b = false;
+    c = false;
+    Print(a || b);
+    Print(b || c);
+    Print(a && b);
+    Print(c && b);
+    Print(a && true);
+    Print(a && false);
+    Print(a == b);
+    Print(b == c);
+}
---
+++ Tests/expr_int_operation.d
@@ -0,0 +1,17 @@
+int main() {
+    int a;
+    int b;
+    int c;
+    a = 7;
+    b = 13;
+    c = 3;
+    Print(a*c);
+    Print(a/3);
+    Print(1/3);
+    Print(13/7);
+    Print(7%13);
+    Print(13%7);
+    Print(a+b);
+    Print(b-a);
+    Print(a-b);
+}
